56edc900792625f98052d3a0c3a7f3871447b481
Cloudflare233 <CLoudflare233@yandex.com>
2022-08-17 17:22:07 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { useState } from "react";
 import { useTheme } from "next-themes";
-import Link from 'next/link'
+import Link from "next/link";
 import Player from "../components/Player";
 
 const data = [
@@ -209,18 +209,9 @@ export default function Home() {
               </div>
             </Link>
             <button onClick={() => setDonate(false)}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 absolute -mt-32 right-4"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
-                  clipRule="evenodd"
-                />
-              </svg>
+              <span className="absolute -mt-36 right-8 text-sm  sm:text-base opacity-50">
+                [x]
+              </span>
             </button>
           </div>
         )}
